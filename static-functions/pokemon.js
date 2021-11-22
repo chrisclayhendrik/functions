06f5b9225fa94be62b614ed70040e7e50ed3880f
Chris <chrisclayhendrik@gmail.com>
2021-11-22 11:12:16 -0600

    Completed Pokemon

--- static-functions/pokemon.js
+++ static-functions/pokemon.js
@@ -6,13 +6,25 @@ class Pokemon {
         this.hp = hp;
     }
 
-    static isValidPokemon(name, number, type, hp) {
-        return name && number && type && hp;
-    }
-
     static isValidPokemonType(type) {
-        return type === 'fire' || type === 'water' || type === 'grass';
+        return type === 'fire' || type === 'water' || type === 'earth' || type === 'electric';
     }
 
+    static isValidPokemonHP(hp) {
+        return hp > 0;
+    }
 
+    static isValidPokemonNumber(number) {
+        return number > 0 && number < 898;
+    }
+
+    static isValidPokemonName(name) {
+        return name.length > 0;
+    }
+
+    static isValidPokemon (name, number, type, hp) {
+        return this.isValidPokemonName(name) && this.isValidPokemonNumber(number) && this.isValidPokemonType(type) && this.isValidPokemonHP(hp);
+    }
 }
+
+console.log(Pokemon.isValidPokemon('Pikachu', 1, 'fire', 100));
